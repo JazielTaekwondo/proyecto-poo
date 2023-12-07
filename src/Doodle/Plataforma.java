20f35bf6b28ce933a185db197f1670f7e218a53c
Rodrigo <rodrigo.lugofi@gmail.com>
2023-12-06 22:51:19 -0600

    Ya mira a la izquierda y derecha

--- src/Doodle/Plataforma.java
+++ src/Doodle/Plataforma.java
@@ -2,19 +2,25 @@ package Doodle;
 
 import java.awt.*;
 
+import javax.swing.ImageIcon;
+
 //import javax.swing.JFrame;
 
 public class Plataforma {
     private int x;
     private int y;
-    private final int height = 15;
+    private final int height = 20;
     private final int width = 60;
     private boolean enPantalla;
+
+    private ImageIcon plataformaIcon;
 
     public Plataforma(int x, int y) {
         this.x = x;
         this.y = y;
         this.enPantalla=true;
+
+        plataformaIcon = new ImageIcon(getClass().getResource("/images/plataforma.png"));
     }
 
     public int getY(){return y;}
@@ -30,8 +36,10 @@ public class Plataforma {
     }
 
     public void draw(Graphics g) {
-        g.setColor(new Color(100, 255, 100));
-        g.fillRect(x, y, width, height);
+        Image plataformaImage = plataformaIcon.getImage();
+        g.drawImage(plataformaImage, x, y, width, height, null);
+        /*g.setColor(new Color(100, 255, 100));
+        g.fillRect(x, y, width, height);*/
     }
 
     public boolean getEnPantalla(){
